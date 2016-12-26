350124e251f600ee73493716448a2a963a57a08f
Tauqeer Ahmad <tauqeer.ahmad2008@gmail.com>
2016-12-26 19:04:32 +0500

    Removed unused code

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,6 @@ import Header from './Header'
 export default class Layout extends Component {
 
   render() {
-    console.log(this.props)
     const childrenWithProps = React.Children.map(this.props.children,
      (child) => React.cloneElement(child, this.props)
     )
